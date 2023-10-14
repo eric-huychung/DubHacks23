6e488491ca048868b0a71e827905cc8ff5895601
Kwon-Yu Chen <kchen04@uw.edu>
2023-10-14 14:14:05 -0700

    initial setup

---
+++ dubhacks23/src/Sections/Container.jsx
@@ -0,0 +1,8 @@
+const { default: styled } = require('styled-components');
+
+export const Container = styled.section`
+	max-width: 1200px;
+	min-width: 320px;
+	margin: 0 auto;
+	padding: 0px 1rem;
+`;
