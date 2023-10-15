0d1a5ebff5787fc689e29c02b7cff26f64e21e98
Kwon-Yu Chen <kchen04@uw.edu>
2023-10-14 21:57:04 -0700

    lower case

--- dubhacks23/src/Sections/Container.jsx
+++
@@ -1,8 +0,0 @@
-const { default: styled } = require('styled-components');
-
-export const Container = styled.section`
-	max-width: 1200px;
-	min-width: 320px;
-	margin: 0 auto;
-	padding: 0px 1rem;
-`;
